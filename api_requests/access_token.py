1dd39c36a9d2c7a231e5933cbeb834209e68c353
Eduard Zakharov <eduard.zakharov@vrpconsulting.com>
2020-08-30 14:12:39 +0300

    pass change

--- api_requests/access_token.py
+++ api_requests/access_token.py
@@ -5,7 +5,7 @@ params_post = {
     "client_id": "YjkyMzJmNDUzMGNmOTYzODVlNDQ0ZjE2OTUxOTU1NDI5MGViN2I3NWU3YTFjNmY300D6F000002JvCk",
     "client_secret": "3490590311975058371",
     "username": "ed@ed.ed",
-    "password": "Cheglad3e|"
+    "password": "Cheglad3e]"
     }
 
 get_access_token = requests.post("https://login.salesforce.com/services/oauth2/token", params=params_post)
